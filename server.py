06ac9f56d75707fa2a59cb2b09ee8457e9aab1bf
Elle Pujals <ellepujals@gmail.com>
2018-10-22 19:40:13 +0000

    Created server.py and requirements file

---
+++ server.py
@@ -0,0 +1,23 @@
+"""File that deals with server."""
+
+from jinja2 import StrictUndefined
+
+from flask import (Flask, render_template, redirect, request, flash, 
+                    session, Markup)
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import * 
+
+
+app = Flask(__name__) # this is the instance of the flask application
+
+app.secret_key = "ABC" # Required to use Flask sessions and debug toolbar
+
+app.jinja_env.undefined = StrictUndefined # Raises error for undefined vars
+
+
+## create all the routes here #########################################
+## @app.route('/')
+    #def index():
+    """Homepage.""" 
