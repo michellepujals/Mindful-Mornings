b4fc0c7daab855d6bee5893688116f07a60cc752
vagrant <vagrant@vagrant.vm>
2018-10-23 00:53:48 +0000

    Class method working, refactoring

--- model.py
+++ model.py
@@ -14,12 +14,6 @@ class User(db.Model):
 
     __tablename__ = "users"
 
-    def __init__(self, username, password):
-        """Create a user, given username and password."""
-
-        self.username = username
-        self.password = password
-
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
@@ -29,16 +23,37 @@ class User(db.Model):
 
     tasks = db.relationship('Task', backref='user')
 
+    def __init__(self, username, password):
+        """Create a user, given username and password."""
+
+        self.username = username
+        self.password = password
+
+
     def __repr__(self):
         """Provide helpful representation when printed."""
 
         return f"<User user_id={self.user_id} username={self.username}>"
 
 
+    @classmethod
+    def get_user_by_username(cls, username):
+        """Get a user, given their username."""
+
+        return cls.query.filter_by(username=username).one()
+
+    
 class Task(db.Model):
     """Tasks owned by users of Mindful Mornings website."""
 
     __tablename__ = "tasks"
+
+    def __init__(self, user_id, task_name, duration_estimate):
+        """Create a task, given user_id and task name."""
+
+        self.user_id = user_id
+        self.task_name = task_name
+        self.duration_estimate = duration_estimate
 
     task_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"),
