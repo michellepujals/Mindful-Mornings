5da2efcec90a0e72008d685f9c37552663f5efdd
vagrant <vagrant@vagrant.vm>
2018-11-06 01:46:23 +0000

    Google maps route working to display different transit options

--- static/js/dashboard.js
+++
@@ -1,7 +0,0 @@
-/* Divide whole thing into a component, then subdivide into 3 components for 
-the 3 colunms, then keep subdividing until down to the smallest components 
-(ie a task is a component and can be reused) */
-
-
-//Map code//
-let eastOakland = {}
