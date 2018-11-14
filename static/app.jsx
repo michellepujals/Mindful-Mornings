cd587152e5b9477b0c8cdccf8dff248c499b1850
Elle Pujals <ellepujals@gmail.com>
2018-11-14 23:56:26 +0000

    File cleanup

---
+++ static/app.jsx
@@ -0,0 +1,6 @@
+/* This is the wrapper component for the entire app*/
+class Hello extends React.Component {
+  render() {
+    return <p>Hi World!</p>;
+  }
+}
---
+++ static/index.jsx
@@ -0,0 +1,2 @@
+//Puts the App into the DOM
+ReactDOM.render(<Hello />, document.getElementById('root'));
