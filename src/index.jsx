686b40ddf74d1b9a0259e5c2ac9915e5eb008b1f
vagrant <vagrant@vagrant.vm>
2018-11-14 07:05:42 +0000

    Refactoring React components

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './dashboard.jsx';
+import 'atlaskit/css-reset';
+import App from './app.jsx';
 
 window.React = React;
 window.ReactDOM = ReactDOM;
 window.App = App;
+
+//Puts the App into the DOM
+ReactDOM.render(<App />, document.getElementById('root'));
